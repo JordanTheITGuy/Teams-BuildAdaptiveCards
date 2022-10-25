f952b19d44f9ecb87e71948b347a359ba66b616d
JordanTheITGuy <jordan.benzing@gmail.com>
2022-10-25 00:27:14 +0000

    Fixed Test for Mentions

--- UnitTests/Test-Mentions.ps1
+++ UnitTests/Test-Mentions.ps1
@@ -3,7 +3,7 @@
 Describe TeamsMentions {
 
     It "Should always be of type mentionEntity"{
-         $result = New-TeamsMention -UPN jordan@domain.com -FullName "Jordan Benzing"
+         $result = New-SingleTeamsMention -UPN jordan@domain.com -FullName "Jordan Benzing"
          $result | Should -Be mentionEntity
     }
 }
